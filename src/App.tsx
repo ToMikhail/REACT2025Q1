7c61fb1f1e77449e62e5299c0e9ab289dd52dc92
Toustsik Mikahil <toustsikmikhail@gmail.com>
2025-02-02 16:04:57 +0100

    feat: add fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
+import Button from './components/Button';
 import Results from './components/Results';
 import TopControls from './components/TopControls';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
       <TopControls />
-      <Results />
+      <main className="main">
+        <Results />
+        <Button type="submit" name="Error" />
+      </main>
     </div>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
 
-// interface ResultsProps {}
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-// interface ResultsState {}
+interface State {
+  users: User[];
+  loading: boolean;
+  error: string | null;
+}
 
-class Results extends React.Component {
+class Results extends React.Component<unknown, State> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
+      .then((data: User[]) => {
+        console.log(data);
+        this.setState({ users: data.results, loading: false });
+      })
+
+      .catch((error) =>
+        this.setState({ error: error.message, loading: false })
+      );
+  }
+
   render() {
-    return <h2>Results works!</h2>;
+    const { users, loading, error } = this.state;
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return (
+      <div>
+        <h2>User List</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
